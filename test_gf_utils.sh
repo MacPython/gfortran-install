d34996330aac7c922cb340347dcb6fc3fc1a993b
Matthew Brett <matthew.brett@gmail.com>
2016-06-26 00:40:11 -0700

    Fix to tests for uname

--- test_gf_utils.sh
+++ test_gf_utils.sh
@@ -1,19 +1,20 @@
 # Get gfortran utils
 source gfortran_utils.sh
 
-if [ $(uname) == Darwin ]; then
+uname=$(uname)
+if [ $uname == Darwin ]; then
     install_gfortran
     [ -n "$GFORTRAN_DMG" ] || (echo "GFORTRAN_DMG not set"; exit 1)
     [ -n "$GFORTRAN_SHA" ] || (echo "GFORTRAN_SHA not set"; exit 1)
     SUF="-1becaaa"
-elif [ $(uname) == Linux ]; then
+elif [ $uname == Linux ]; then
     [ -z "$GFORTRAN_DMG" ] || (echo "GFORTRAN_DMG is set"; exit 1)
     [ -z "$GFORTRAN_SHA" ] || (echo "GFORTRAN_SHA is set"; exit 1)
     # Check check, if gfortran installed
-    gfortran --help > /dev/null && check_gfortran
+    gfortran --help > /dev/null && check_gfortrangfortran --help > /dev/null && check_gfortran
     SUF=""
 fi
-exp_64_tgz="archives/openblas-0.2.18-Darwin-x86_64${SUF}.tar.gz"
+exp_64_tgz="archives/openblas-0.2.18-${uname}-x86_64${SUF}.tar.gz"
 [ "$(get_gf_lib_for_suf "${SUF}" "openblas-0.2.18" "x86_64")" == "$exp_64_tgz" ] || \
     (echo Wrong tgz output; exit 1)
 [ -f "$exp_64_tgz" ] || (echo Failed archive fetch; exit 1)
@@ -21,7 +22,7 @@ exp_64_tgz="archives/openblas-0.2.18-Darwin-x86_64${SUF}.tar.gz"
 [ "$(get_gf_lib_for_suf "${SUF}" "openblas-0.2.18" "x86_64")" == "$exp_64_tgz" ] || \
     (echo Wrong tgz output; exit 1)
 # Check another archive
-exp_32_tgz="archives/openblas-0.2.18-Darwin-i686${SUF}.tar.gz"
+exp_32_tgz="archives/openblas-0.2.18-${uname}-i686${SUF}.tar.gz"
 [ "$(get_gf_lib_for_suf "${SUF}" "openblas-0.2.18" "i686")" == "$exp_32_tgz" ] || \
     (echo Wrong tgz output; exit 1)
 [ -f "$exp_32_tgz" ] || (echo Failed archive fetch; exit 1)
