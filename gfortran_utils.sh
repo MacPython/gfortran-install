4929657654bcb459eab7c1a107c10daddb1d361f
Matthew Brett <matthew.brett@gmail.com>
2016-06-26 00:33:29 -0700

    Full path to DMG; fix for function name

--- gfortran_utils.sh
+++ gfortran_utils.sh
@@ -1,7 +1,9 @@
 # Bash utilities for use with gfortran
 
-LIB_URL="https://3f23b170c54c2533c070-1c8a9b3114517dc5fe17b7c3f8c63a43.ssl.cf2.rackcdn.com"
+GF_LIB_URL="https://3f23b170c54c2533c070-1c8a9b3114517dc5fe17b7c3f8c63a43.ssl.cf2.rackcdn.com"
 ARCHIVE_SDIR="${ARCHIVE_SDIR:-archives}"
+
+GF_UTIL_DIR=$(dirname "${BASH_SOURCE[0]}")
 
 function check_gfortran {
     # Check that gfortran exists on the path
@@ -20,14 +22,14 @@ function get_gf_lib_for_suf {
     local fname="$prefix-${uname}-${plat}${suffix}.tar.gz"
     local out_fname="${ARCHIVE_SDIR}/$fname"
     if [ ! -e "$out_fname" ]; then
-        curl -L "${LIB_URL}/$fname" > $out_fname || (echo "Fetch failed"; exit 1)
+        curl -L "${GF_LIB_URL}/$fname" > $out_fname || (echo "Fetch failed"; exit 1)
     fi
     echo "$out_fname"
 }
 
 if [ "$(uname)" == "Darwin" ]; then
     export MACOSX_DEPLOYMENT_TARGET="10.6"
-    GFORTRAN_DMG="archives/gfortran-4.9.0-Mavericks.dmg"
+    GFORTRAN_DMG="${GF_UTIL_DIR}/archives/gfortran-4.9.0-Mavericks.dmg"
     GFORTRAN_SHA="$(shasum $GFORTRAN_DMG)"
 
     function install_gfortran {
@@ -48,6 +50,6 @@ else
 
     function get_gf_lib {
         # Get library with no suffix
-        get_gf_lib_for_suff "" $@
+        get_gf_lib_for_suf "" $@
     }
 fi
