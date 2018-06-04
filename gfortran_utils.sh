994103f768accaf3528f452b765d97d87d627796
Matthew Brett <matthew.brett@gmail.com>
2018-06-04 11:00:56 +0100

    Refactor archive names

--- gfortran_utils.sh
+++ gfortran_utils.sh
@@ -5,6 +5,35 @@ ARCHIVE_SDIR="${ARCHIVE_SDIR:-archives}"
 
 GF_UTIL_DIR=$(dirname "${BASH_SOURCE[0]}")
 
+function get_distutils_platform {
+    # Report platform as in form of distutils get_platform.
+    # This is like the platform tag that pip will use.
+    # Modify fat architecture tags on macOS to reflect compiled architecture
+    local plat=$1
+    case $plat in
+        i686|x86_64|intel) ;;
+        *) echo Did not recognize plat $plat; return 1 ;;
+    esac
+    local uname=${2:-$(uname)}
+    if [ "$uname" != "Darwin" ]; then
+        if [ "$plat" == "intel" ]; then
+            echo plat=intel not allowed for Manylinux
+            return 1
+        fi
+        echo "manylinux1_$plat"
+        return
+    fi
+    # macOS 32-bit arch is i386
+    [ "$plat" == "i686" ] && plat="i386"
+    local target=$(echo $MACOSX_DEPLOYMENT_TARGET | tr .- _)
+    echo "macosx_${target}_${plat}"
+}
+
+function get_macosx_target {
+    # Report MACOSX_DEPLOYMENT_TARGET as given by distutils get_platform.
+    python -c "import sysconfig as s; print(s.get_config_vars()['MACOSX_DEPLOYMENT_TARGET'])"
+}
+
 function check_gfortran {
     # Check that gfortran exists on the path
     if [ -z "$(which gfortran)" ]; then
@@ -19,7 +48,9 @@ function get_gf_lib_for_suf {
     local plat=${3:-$PLAT}
     local uname=${4:-$(uname)}
     if [ -z "$prefix" ]; then echo Prefix not defined; exit 1; fi
-    local fname="$prefix-${uname}-${plat}${suffix}.tar.gz"
+    local plat_tag=$(get_distutils_platform $plat $uname)
+    if [ -n "$suffix" ]; then suffix="-$suffix"; fi
+    local fname="$prefix-${plat_tag}${suffix}.tar.gz"
     local out_fname="${ARCHIVE_SDIR}/$fname"
     if [ ! -e "$out_fname" ]; then
         curl -L "${GF_LIB_URL}/$fname" > $out_fname || (echo "Fetch failed"; exit 1)
@@ -28,7 +59,8 @@ function get_gf_lib_for_suf {
 }
 
 if [ "$(uname)" == "Darwin" ]; then
-    export MACOSX_DEPLOYMENT_TARGET="10.6"
+    mac_target=${MACOSX_DEPLOYMENT_TARGET:-$(get_macosx_target)}
+    export MACOSX_DEPLOYMENT_TARGET=$mac_target
     GFORTRAN_DMG="${GF_UTIL_DIR}/archives/gfortran-4.9.0-Mavericks.dmg"
     GFORTRAN_SHA="$(shasum $GFORTRAN_DMG)"
 
@@ -40,7 +72,7 @@ if [ "$(uname)" == "Darwin" ]; then
 
     function get_gf_lib {
         # Get lib with gfortran suffix
-        get_gf_lib_for_suf "-${GFORTRAN_SHA:0:7}" $@
+        get_gf_lib_for_suf "gf_${GFORTRAN_SHA:0:7}" $@
     }
 else
     function install_gfortran {
--- test_gf_utils.sh
+++ test_gf_utils.sh
@@ -2,39 +2,56 @@
 source gfortran_utils.sh
 
 uname=$(uname)
+FNAME_ROOT="openblas-v0.3.0"
+export MACOSX_DEPLOYMENT_TARGET=10.6
+ARCH_ROOT="archives/$FNAME_ROOT"
 if [ $uname == Darwin ]; then
-    install_gfortran
-    [ -n "$GFORTRAN_DMG" ] || (echo "GFORTRAN_DMG not set"; exit 1)
-    [ -n "$GFORTRAN_SHA" ] || (echo "GFORTRAN_SHA not set"; exit 1)
-    SUF="-1becaaa"
+    # install_gfortran
+    # [ -n "$GFORTRAN_DMG" ] || (echo "GFORTRAN_DMG not set"; exit 1)
+    # [ -n "$GFORTRAN_SHA" ] || (echo "GFORTRAN_SHA not set"; exit 1)
+    GFORTRAN_SHA=1becaaa76fe86f86df35f89ca30446c14a072d41
+    target=$(echo $MACOSX_DEPLOYMENT_TARGET | tr .- _)
+    PLAT_ROOT="macosx_$target"
+    I686_PLAT="i386"
+    SUF="gf_${GFORTRAN_SHA:0:7}"
 elif [ $uname == Linux ]; then
     [ -z "$GFORTRAN_DMG" ] || (echo "GFORTRAN_DMG is set"; exit 1)
     [ -z "$GFORTRAN_SHA" ] || (echo "GFORTRAN_SHA is set"; exit 1)
     # Check check, if gfortran installed
-    gfortran --help > /dev/null && check_gfortrangfortran --help > /dev/null && check_gfortran
+    gfortran --help > /dev/null && check_gfortran
+    PLAT_ROOT="manylinux1_"
+    I686_PLAT="i686"
     SUF=""
 fi
-exp_64_tgz="archives/openblas-0.2.18-${uname}-x86_64${SUF}.tar.gz"
-[ "$(get_gf_lib_for_suf "${SUF}" "openblas-0.2.18" "x86_64")" == "$exp_64_tgz" ] || \
+if [ -n "$SUF" ]; then SUFP="-$SUF"; else SUFP=""; fi
+exp_64_tgz="${ARCH_ROOT}-${PLAT_ROOT}_x86_64${SUFP}.tar.gz"
+[ "$(get_gf_lib_for_suf "${SUF}" "${FNAME_ROOT}" "x86_64")" == "$exp_64_tgz" ] || \
     (echo Wrong tgz output; exit 1)
 [ -f "$exp_64_tgz" ] || (echo Failed archive fetch; exit 1)
 # Refetch does not raise error
-[ "$(get_gf_lib_for_suf "${SUF}" "openblas-0.2.18" "x86_64")" == "$exp_64_tgz" ] || \
+[ "$(get_gf_lib_for_suf "${SUF}" "${FNAME_ROOT}" "x86_64")" == "$exp_64_tgz" ] || \
     (echo Wrong tgz output; exit 1)
 # Check another archive
-exp_32_tgz="archives/openblas-0.2.18-${uname}-i686${SUF}.tar.gz"
-[ "$(get_gf_lib_for_suf "${SUF}" "openblas-0.2.18" "i686")" == "$exp_32_tgz" ] || \
+exp_32_tgz="${ARCH_ROOT}-${PLAT_ROOT}_${I686_PLAT}${SUFP}.tar.gz"
+[ "$(get_gf_lib_for_suf "${SUF}" "${FNAME_ROOT}" "i686")" == "$exp_32_tgz" ] || \
     (echo Wrong tgz output; exit 1)
 [ -f "$exp_32_tgz" ] || (echo Failed archive fetch; exit 1)
 # Refetch does not raise error
-[ "$(get_gf_lib_for_suf "${SUF}" "openblas-0.2.18" "i686")" == "$exp_32_tgz" ] || \
+[ "$(get_gf_lib_for_suf "${SUF}" "${FNAME_ROOT}" "i686")" == "$exp_32_tgz" ] || \
     (echo Wrong tgz output; exit 1)
+# Check intel (fused)
+if [ $uname == Darwin ]; then
+    exp_fused_tgz="${ARCH_ROOT}-${PLAT_ROOT}_intel${SUFP}.tar.gz"
+    [ "$(get_gf_lib_for_suf "${SUF}" "${FNAME_ROOT}" "intel")" == "$exp_fused_tgz" ] || \
+        (echo Wrong tgz output; exit 1)
+    [ -f "$exp_fused_tgz" ] || (echo Failed archive fetch; exit 1)
+fi
 # Same thing, setting the hash manually, for OSX
 GFORTRAN_SHA="1becaaa76fe86f86df35f89ca30446c14a072d41"
 rm $exp_64_tgz $exp_32_tgz
-[ "$(get_gf_lib "openblas-0.2.18" "x86_64")" == "$exp_64_tgz" ] || \
+[ "$(get_gf_lib "${FNAME_ROOT}" "x86_64")" == "$exp_64_tgz" ] || \
     (echo Wrong tgz output; exit 1)
 [ -f "$exp_64_tgz" ] || (echo Failed archive fetch; exit 1)
-[ "$(get_gf_lib "openblas-0.2.18" "i686")" == "$exp_32_tgz" ] || \
+[ "$(get_gf_lib "${FNAME_ROOT}" "i686")" == "$exp_32_tgz" ] || \
     (echo Wrong tgz output; exit 1)
 [ -f "$exp_32_tgz" ] || (echo Failed archive fetch; exit 1)
